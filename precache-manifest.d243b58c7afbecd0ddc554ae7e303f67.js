1121d5dd1d9728a969827e48045a4a08db74c590
Lavish <lavishsingla0001@gmail.com>
2019-10-10 15:12:36 +0530

    Updates

---
+++ precache-manifest.d243b58c7afbecd0ddc554ae7e303f67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d0db2ffca707153b72f5e9c6c6c56b",
+    "url": "/contentstoreforwhitepanda/index.html"
+  },
+  {
+    "revision": "880f6f510e3081584892",
+    "url": "/contentstoreforwhitepanda/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "b915d2f7249f60a842bc",
+    "url": "/contentstoreforwhitepanda/static/css/main.8a201f58.chunk.css"
+  },
+  {
+    "revision": "880f6f510e3081584892",
+    "url": "/contentstoreforwhitepanda/static/js/2.2ce82c7e.chunk.js"
+  },
+  {
+    "revision": "b915d2f7249f60a842bc",
+    "url": "/contentstoreforwhitepanda/static/js/main.a2fad3e9.chunk.js"
+  },
+  {
+    "revision": "5b57531ac9a3515ae6d6",
+    "url": "/contentstoreforwhitepanda/static/js/runtime-main.fc88222f.js"
+  },
+  {
+    "revision": "483f6b5683140e7df7f19482adcdb7fe",
+    "url": "/contentstoreforwhitepanda/static/media/avatar.483f6b56.png"
+  }
+]);
